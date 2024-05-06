1bea9e572bff98975c8cd14f5bacb4b302740700
farsuller <farsuller2020@gmail.com>
2024-05-06 18:27:46 +0800

    release vcode 2 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
 
     defaultConfig {
         applicationId = "com.quotesapp.quotesko"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
     }
 
